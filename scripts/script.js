d40646ce02c6f97fbeafb9e34e5743a011c3a703
Walid Djellali <walidbibo1997@gmail.com>
2023-05-24 20:29:20 +0100

    add jour and chargement

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 
-// import tabJoursEnOrdre from "./Utilitaire/gestionTemps.js";
+import tabJoursEnOrdre from "./Utilitaire/gestionTemps.js";
 
 let resultasApi;
 
@@ -13,12 +13,14 @@ const localisation = document.querySelector('.localisation');
 const heure = document.querySelectorAll('.heure-nom-prevision');
 const temppourH= document.querySelectorAll ('.heure-prevision-valeur');
 const imgIcon = document.querySelector('.logo-meteo');
-const iconUrlD = "//cdn.weatherapi.com/weather/64x64/day/116.png";
-const iconUrlN = "//cdn.weatherapi.com/weather/64x64/night/116.png";
+const iconUrlD = '//cdn.weatherapi.com/weather/64x64/day/116.png';
+const iconUrlN = '//cdn.weatherapi.com/weather/64x64/night/116.png';
 
 
-// const jourDiv = document.querySelectorAll ('.jour-prevision-nom');
-// const tempJoursdiv = document.querySelectorAll('.jour-prevision-temp');
+const jourDiv = document.querySelectorAll ('.jour-prevision-nom');
+const tempJoursdiv = document.querySelectorAll('.jour-prevision-temp');
+
+const chargemenContainer = document.querySelector('.overlay-icon-chargement');
 
 if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -34,7 +36,7 @@ if(navigator.geolocation) {
 }
 
 function AppelApi(long, lat) {
-    fetch(`https://api.weatherapi.com/v1/forecast.json?key=507790bcedeb4b65ae8125129232405&q=oran&days=1&aqi=no&alerts=no&lang=fr`)
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=507790bcedeb4b65ae8125129232405&q=oran&days=7&aqi=no&alerts=no&lang=fr`)
     .then((reponse)=> {
         return reponse.json();
     })
@@ -72,23 +74,26 @@ function AppelApi(long, lat) {
            temppourH[j].innerText = `${resultasApi.forecast.forecastday[0].hour[j * 3].temp_c}°`
          }
 
-        //  for (let k = 0; k < tabJoursEnOrdre.length; k++) {
-        //     jourDiv[k].innerText = tabJoursEnOrdre[k].slice(0,3);
+         for (let k = 0; k < tabJoursEnOrdre.length; k++) {
+             jourDiv[k].innerText = tabJoursEnOrdre[k].slice(0,3);
             
-        //  }
+        }
         //  // temps par jour 
-        // //  for(let m=0; m < 7; m++){
-        // //     tempJoursdiv[m].innerText = `${resultasApi.forecast.forecastday.day[m + 1].temp_c}°`
-        // //  }
+         for(let m=0; m < 7; m++){
+             tempJoursdiv[m].innerText = `${resultasApi.forecast.forecastday[m].day.avgtemp_c}°`
+     }
     
 
         // icon dynamic 
-        if(heurAccuelle > 6 && heurAccuelle <21) {
+        if(heurAccuelle > 6 && heurAccuelle <20) {
             imgIcon.src = iconUrlD;
 
         }else {
             imgIcon.src = iconUrlN;
         }
+
+        chargemenContainer.classList.add('disparition');
+
     })
 
 }
